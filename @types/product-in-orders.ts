52c3fc94968fdcaebb44379e1530b98295014303
My Little Pet <mylittlepetinternal@gmail.com>
2024-10-15 15:32:19 -0300

    feat: add types ProductInOrderList

--- @types/product-in-orders.ts
+++ @types/product-in-orders.ts
@@ -1,8 +1,16 @@
 export interface OrderItem {
-    id: string;          // ID do item do pedido
-    productId: string;   // ID do produto associado a este item
-    quantity: number;    // Quantidade do produto no pedido
-    orderId: string;     // ID do pedido ao qual este item pertence
-    created_at: string;  // Data de criação do item (formato ISO 8601)
-    updated_at: string;  // Data da última atualização do item (formato ISO 8601)
-  }
+  id: string;          // ID do item do pedido
+  productId: string;   // ID do produto associado a este item
+  quantity: number;    // Quantidade do produto no pedido
+  orderId: string;     // ID do pedido ao qual este item pertence
+  created_at: string;  // Data de criação do item (formato ISO 8601)
+  updated_at: string;  // Data da última atualização do item (formato ISO 8601)
+}
+
+export interface ProductInOrderList {
+  id: string;
+  name: string;
+  image: string;
+  price: number;
+  quantity: number;
+}
